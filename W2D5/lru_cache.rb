4633d5c18565c1b7fb7a62ab044479b0d2408680
Edwin Shin <edwinshin@gmail.com>
2017-12-15 08:03:30 -0800

    Implement lru cache

--- W2D5/lru_cache.rb
+++ W2D5/lru_cache.rb
@@ -1,20 +1,39 @@
 class LRUCache
-    def initialize
+    def initialize(size)
+      @array = Array.new(4)
     end
 
     def count
       # returns number of elements currently in cache
+      @array.size
     end
 
     def add(el)
       # adds element to cache according to LRU principle
+      if @array.include?(el)
+        check_recent(el)
+      else
+        if count < 4
+          @array.push(el)
+        else
+          @array.shift
+          @array.push(el)
+        end
+      end
     end
 
     def show
       # shows the items in the cache, with the LRU item first
+      p @array
     end
 
     private
     # helper methods go here!
+    def check_recent(el)
 
+        idx = @array.index(el)
+        @array.delete_at(idx)
+        @array.push(el)
+
+    end
   end
