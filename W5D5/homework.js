48b03099e0d7dbc8fe0ed1390f933ff75205378a
Edwin Shin <edwinshin@gmail.com>
2018-01-12 07:19:12 -0800

    W5D5 homework

---
+++ W5D5/homework.js
@@ -0,0 +1,10 @@
+
+window.setTimeout(function() {alert("HAMMERTIME"); }, 5000);
+
+
+const hammerTime = function(time) {
+  window.setTimeout(function() {alert("HAMMERTIME"); }, time);
+};
+
+hammerTime(5000);
+
---
+++ W5D5/node.js
@@ -0,0 +1,39 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// reader.question("Would you like some tea?", function (firstAns) {
+//   console.log(firstAns);
+//
+//   reader.question("Would you like some biscuits?", function(secondAns) {
+//     console.log(`So you ${firstAns} want tea and you ${secondAns} want biscuits.`);
+//     reader.close();
+//   });
+//
+// });
+
+
+
+function teaAndBiscuits () {
+  let first, second;
+
+  reader.question('Would you like some tea?', (res) => {
+    first = res;
+    console.log(`You replied ${res}.`);
+
+    reader.question('Would you like some biscuits?', (res2) => {
+      second = res2;
+      console.log(`You replied ${res2}.`);
+      const firstRes = (first === 'yes') ? 'do' : 'don\'t';
+      const secondRes = (second === 'yes') ? 'do' : 'don\'t';
+      console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`);
+      reader.close();
+    });
+  });
+}
+
+teaAndBiscuits();
+
