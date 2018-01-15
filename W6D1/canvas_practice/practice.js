4580375ef782116a532c9993ea072a7311c6bdaf
Edwin Shin <edwinshin@gmail.com>
2018-01-15 00:54:36 -0800

    W6D1 homework

---
+++ W6D1/canvas_practice/practice.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const canvas = document.getElementById('mycanvas'); // in your HTML this element appears as <canvas id="myCanvas"></canvas>
+  const ctx = canvas.getContext('2d');
+  canvas.width = 500;
+  canvas.height = 500;
+
+  ctx.fillStyle = 'rgb(200,0,0)';
+  ctx.fillRect(10, 10, 55, 50);
+
+  ctx.beginPath();
+  ctx.arc(100, 100, 100, 0, 2*Math.PI);
+  ctx.strokeStyle = "blue";
+  ctx.lineWidth = 1;
+  ctx.stroke();
+
+  ctx.fillStyle = "red";
+  ctx.fill();
+});
