1357561ede3a0e27f8cb6e5c1670ab5d58d1f99d
Edwin Shin <edwinshin@gmail.com>
2018-01-17 21:30:39 -0800

    W6D4 Homework

--- W6D3/skeleton/js/index.js
+++ W6D3/skeleton/js/index.js
@@ -5,8 +5,8 @@ $.ajax({
   type: 'GET',
   url: 'http://api.openweathermap.org/data/2.5/weather' +
   	'?q=London,uk&appid=bcb83c4b54aee8418983c2aff3073b3b',
-  success(data) {
-    console.log(data);
+  success(weather) {
+    console.log(weather);
   },
   error() {
     console.error("An error occurred.");
